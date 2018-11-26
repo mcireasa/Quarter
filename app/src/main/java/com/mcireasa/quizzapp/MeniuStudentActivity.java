f100cb2b62a303c131e449a02c0e5aba74dfae60
amaliacornea <amaliacornea97@gmail.com>
2018-11-26 20:28:27 +0200

    live @mara's home

---
+++ app/src/main/java/com/mcireasa/quizzapp/MeniuStudentActivity.java
@@ -0,0 +1,31 @@
+package com.mcireasa.quizzapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MeniuStudentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meniu_student);
+    }
+
+    public void onClickSearch(View view)
+    {
+        Intent explicitIntent =
+                new Intent(this,
+                        SearchActivity.class);
+        startActivity(explicitIntent);
+    }
+
+    public void onClickStartTest(View view)
+    {
+        Intent explicitIntent =
+                new Intent(this,
+                        StartTest.class);
+        startActivity(explicitIntent);
+    }
+}
