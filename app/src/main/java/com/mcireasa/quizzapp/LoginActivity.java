c7934e2b318d45d3d6675c9f7cbb11a87b8ecc89
Constantinescu <mconsta5@techteam.com>
2019-01-11 13:30:05 +0200

    createAccount

--- app/src/main/java/com/mcireasa/quizzapp/LoginActivity.java
+++ app/src/main/java/com/mcireasa/quizzapp/LoginActivity.java
@@ -44,6 +44,12 @@ public class LoginActivity extends AppCompatActivity {
         dialog.show();
     }
 
+    public void onClickNewAccount(View view){
+        Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
+        startActivity(intent);
+
+    }
+
 
     public void OnClikLoginBtn(View view) {
         final EditText textUsername = (EditText)findViewById(R.id.TextUsername);
